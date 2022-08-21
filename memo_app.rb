6626ce9259d21b5509b5735f95ca12f7582b1e49
maimux2x <m0smile10often@gmail.com>
2022-08-21 14:43:09 +0900

    レビューいただいた内容の修正
    initializeメソッドの初期値の修正、それに伴う関連コードの見直し、SQLにクエリを投げる際にexec_paramsを使うように修正

--- memo_app.rb
+++ memo_app.rb
@@ -12,43 +12,32 @@ set :show_exceptions, :after_handler
 
 # メモのインスタンスを作成し保存
 class BaseMemosData
-  attr_accessor :params, :id, :created_at
+  attr_accessor :title, :memo_desc, :id, :created_at
 
-  def initialize(params)
+  def initialize(title, memo_desc)
     @id = SecureRandom.uuid
+    @title = title
+    @memo_desc = memo_desc
     @created_at = Time.new
-    @params = params
   end
 
-  def self.register(id, params, created_at)
-    CONNECTION.prepare(
-      'insert_memo',
-      "insert into #{TABLE} (id, title, memo_desc, created_at) values ($1, $2, $3, $4)"
-    )
-    CONNECTION.exec_prepared(
-      'insert_memo',
-      [id, params['title'].to_s, params['memo_desc'].to_s, created_at]
+  def self.register(id, title, memo_desc, created_at)
+    CONNECTION.exec_params(
+      "insert into #{TABLE} values ($1, $2, $3, $4)",
+      [id, title, memo_desc, created_at]
     )
   end
 
-  def self.update(params)
-    CONNECTION.prepare(
-      'update_memo',
-      "update #{TABLE} set title = $1, memo_desc = $2 where id = $3"
-    )
-    CONNECTION.exec_prepared(
-      'update_memo',
-      [params['title'].to_s, params['memo_desc'].to_s, params['id'].to_s]
+  def self.update(title, memo_desc, id)
+    CONNECTION.exec_params(
+      "update #{TABLE} set title = $1, memo_desc = $2 where id = $3",
+      [title, memo_desc, id]
     )
   end
 
-  def self.delete(memo)
-    CONNECTION.prepare(
-      'delete_memo',
-      "delete from #{TABLE} where id = $1"
-    )
-    CONNECTION.exec_prepared(
-      'delete_memo', [memo['id'].to_s]
+  def self.delete(id)
+    CONNECTION.exec_params(
+      "delete from #{TABLE} where id = $1", [id]
     )
   end
 end
@@ -97,9 +86,11 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  raw_memo = BaseMemosData.new(params)
+  raw_memo = BaseMemosData.new(params['title'], params['memo_desc'])
 
-  BaseMemosData.register(raw_memo.id, raw_memo.params, raw_memo.created_at)
+  BaseMemosData.register(
+    raw_memo.id, raw_memo.title, raw_memo.memo_desc, raw_memo.created_at
+  )
 
   redirect '/memos'
 end
@@ -119,14 +110,14 @@ get '/memos/:id/edit' do
 end
 
 patch '/memos/:id' do
-  BaseMemosData.update(params)
+  BaseMemosData.update(params['title'], params['memo_desc'], @memo_data['id'])
   redirect "/memos/#{@memo_data['id']}"
 end
 
 delete '/memos/delete/:id' do
   all_memos = get_memosdata(TABLE)
   all_memos.each do |memo|
-    BaseMemosData.delete(memo) if memo['id'] == params['id']
+    BaseMemosData.delete(memo['id']) if memo['id'] == params['id']
   end
   redirect '/memos'
 end
